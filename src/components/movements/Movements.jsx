6df09ff90c2c7ad0b3128101b4171cdbde590ef4
David <dmg_92@hotmail.com>
2023-10-21 20:07:57 -0600

    mejoras

--- src/components/movements/Movements.jsx
+++ src/components/movements/Movements.jsx
@@ -12,29 +12,38 @@ export const Movements = ({ accountId }) => {
   const [movements, setMovements] = useState([]);
   const [currentDate, setCurrentDate] = useState(new Date());
 
-  const convertDate = (date) => {
+
+  const convertDateToApi = (date) => {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, "0"); // Adding 1 because months are zero-based
     const day = String(date.getDate()).padStart(2, "0");
 
-    // Formatted date in "yyyy-mm-dd" format
     return `${year}-${month}-${day}`;
   };
 
+  const formatDateTime = (dateString) => {
+    const dateObject = new Date(dateString);
+    const formattedDateTime = `${dateObject.getDate()}-${
+      dateObject.getMonth() + 1
+    }-${dateObject.getFullYear()} ${dateObject.getHours()}:${dateObject.getMinutes()}:${dateObject.getSeconds()}`;
+    return formattedDateTime;
+  };
+
+
+  
   useEffect(() => {
     const fetchData = async () => {
-    
       const userString = localStorage.getItem("user");
       const user = JSON.parse(userString);
       axios.defaults.headers.common["Authorization"] = belvoAuth;
 
-      const date_to = convertDate(currentDate);
+      const date_to = convertDateToApi(currentDate);
 
       const dateAMonthAgo = new Date(currentDate);
 
       // Restar un mes
       dateAMonthAgo.setMonth(currentDate.getMonth() - 1);
-      const date_from = convertDate(dateAMonthAgo);
+      const date_from = convertDateToApi(dateAMonthAgo);
       const requestData = {
         link: user.belvo_link,
         token: belvoToken,
@@ -43,7 +52,6 @@ export const Movements = ({ accountId }) => {
         date_to: date_to,
       };
 
-      console.log(requestData)
       try {
         const response = await axios.post(
           belvoUrl + "api/transactions/",
@@ -59,38 +67,35 @@ export const Movements = ({ accountId }) => {
     fetchData();
   }, [accountId]);
 
-  const formatDate = (dateString) => {
-    const dateObject = new Date(dateString);
-    const formattedDate = `${dateObject.getDate()}-${dateObject.getMonth() + 1}-${dateObject.getFullYear()} ${dateObject.getHours()}:${dateObject.getMinutes()}:${dateObject.getSeconds()}`;
-    return formattedDate;
-  };
-
   return (
-    <div className="table-responsive">
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Fecha y hora</th>
-            <th>Cantidad</th>
-            <th>Balance</th>
-            <th>Description</th>
-            <th>Categoria</th>
-            <th>status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {movements.map((movement, index) => (
-            <tr key={index}>
-              <td>{formatDate(movement.created_at)}</td>
-              <td>${movement.amount}</td>
-              <td>${movement.balance}</td>
-              <td>{movement.description}</td>
-              <td>{movement.category}</td>
-              <td>{movement.status}</td>
+    <>
+      <h2>Mis movimientos del ultimo mes</h2>
+      <div className="table-responsive">
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Fecha y hora</th>
+              <th>Cantidad</th>
+              <th>Balance</th>
+              <th>Description</th>
+              <th>Categoria</th>
+              <th>status</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {movements.map((movement, index) => (
+              <tr key={index}>
+                <td>{formatDateTime(movement.created_at)}</td>
+                <td>${movement.amount}</td>
+                <td>${movement.balance}</td>
+                <td>{movement.description}</td>
+                <td>{movement.category}</td>
+                <td>{movement.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
+    </>
   );
 };
