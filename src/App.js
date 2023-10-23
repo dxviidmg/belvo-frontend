29d3c07981de03de2e18968a8751fecf834bb42c
David <dmg_92@hotmail.com>
2023-10-23 11:44:16 -0600

    z

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-//import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import React from "react";
 import { Login } from "./components/login/Login";
-//import { Home } from "./components/home/Home";
+import { Home } from "./components/home/Home";
 
 function App() {
   return (
     <>
-        <Login></Login>
-      {/*<BrowserRouter>
+      *<BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Login />} />
           <Route exact path="/home" element={<Home />} />
         </Routes>
-  </BrowserRouter>*/}
+  </BrowserRouter>
     </>
   );
 }
