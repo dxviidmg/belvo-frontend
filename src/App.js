4943f13d9bd1e64c5a680562ec32a9c686f4a2a1
David <dmg_92@hotmail.com>
2023-10-23 11:13:28 -0600

    del

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from "react";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import React from "react";
 import { Login } from "./components/login/Login";
 import { Home } from "./components/home/Home";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Login />} />
           <Route exact path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
-    </div>
+    </>
   );
 }
 
