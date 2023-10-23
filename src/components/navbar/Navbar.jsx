df4d80cd7e930c262d7b50c461bde5d670d9f9d6
David <dmg_92@hotmail.com>
2023-10-22 19:36:47 -0600

    css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import './navbar.css'
 
 function OffcanvasExample() {
     const expand = false
